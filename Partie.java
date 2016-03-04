bc44302c6ba7549ffacb4376984969b0a32536e1
Max Cabourg <maxcabourg@hotmail.fr>
2016-03-04 19:15:28 +0100

    Mise en place du début de Partie. Gestion de clavier : TODO

--- Partie.java
+++ Partie.java
@@ -8,6 +8,14 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Partie extends World
 {
+    
+    public static final int WIDTH = 600;
+    public static final int HEIGHT = 800;
+    public static final int NOMBRE_LIGNES = 20;
+    public static final int NOMBRE_COLONNES = 20;
+    
+    private int score;
+    private Personnage personnage;
 
     /**
      * Constructor for objects of class Partie.
@@ -15,7 +23,16 @@ public class Partie extends World
      */
     public Partie()
     {    
+
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(WIDTH, HEIGHT, 1);
+        personnage = new Personnage();
+        addObject(personnage, personnage.getX(), personnage.getY()); //On demarre au milieu de la ligne
+        score = 0;
+    }
+    
+    public void act()
+    {
+        personnage.act();
     }
 }
--- Personnage.java
+++ Personnage.java
@@ -12,8 +12,40 @@ public class Personnage extends Actor
      * Act - do whatever the Personnage wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int x;
+    private int y; 
+    
+    public Personnage()
+    {
+        x = Partie.WIDTH/2;
+        y = Partie.HEIGHT;
+    }
+    
     public void act() 
     {
         // Add your action code here.
-    }    
+        gestionClavier();
+    }
+    
+    private void gestionClavier()
+    {
+        if(Greenfoot.isKeyDown("up"))
+            setLocation(getX(), (getY()%Partie.NOMBRE_LIGNES)-(Partie.HEIGHT/Partie.NOMBRE_LIGNES));
+        else if(Greenfoot.isKeyDown("down"))
+            setLocation(getX(), (getY()%Partie.NOMBRE_LIGNES)+(Partie.HEIGHT/Partie.NOMBRE_LIGNES));
+        else if(Greenfoot.isKeyDown("right"))
+            setLocation(getX()+(Partie.WIDTH%Partie.NOMBRE_COLONNES), getY());
+        else if(Greenfoot.isKeyDown("left"))
+            setLocation(getX()-(Partie.WIDTH%Partie.NOMBRE_COLONNES), getY());
+    }
+    
+    public int getX()
+    {
+        return x;
+    }
+    
+    public int getY()
+    {
+        return y;
+    }
 }
